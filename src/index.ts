d4033f08313bf910f387fc0405530f05a7997f39
迷子 (Maiko Tan) <maiko.tan.coding@gmail.com>
2022-07-02 09:14:34 +0000

    refactor: migrate to lodestone-news

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Context } from 'koishi'
 
 import * as i18n from './i18n'
-import { getNews, NewsCategory, NewsRagion } from './utils'
+import { getNews } from './utils'
 
 const categories = [
   'topics',
@@ -20,44 +20,20 @@ export function apply(ctx: Context, config?: {}): void {
   Object.entries(i18n).forEach(([key, value]) => ctx.i18n.define(key, value))
 
   ctx
-    .command('lodestone')
-    .alias('ls')
-    .option('category', '-c <category:string>')
-    .option('ragion', '-r <ragion:string>')
-    .before(({ options, session }) => {
-      if (!options) return
-
-      const { category, ragion } = options
-
-      if (
-        category &&
-        !categories.some((cat) => cat === category || cat.startsWith(category))
-      ) {
-        return session?.text('.invalid_category')
-      }
-      if (
-        ragion &&
-        !ragions.some((rag) => rag === ragion || rag.startsWith(ragion))
-      ) {
-        return session?.text('.invalid_ragion')
-      }
+    .command('lodestone.news')
+    .alias('lsnews')
+    .option('category', '-c <category:string>', {
+      fallback: 'topics',
+    })
+    .option('ragion', '-r <ragion:string>', {
+      fallback: 'jp',
     })
     .action(async ({ options, session }) => {
-      try {
-        const news = (
-          await getNews(
-            ctx,
-            5,
-            (options?.category ?? 'topics') as NewsCategory,
-            options?.ragion as NewsRagion,
-          )
-        )
-          .map((item) => `${item.title}\n${item.url}`)
-          .join('\n\n')
-        return news
-      } catch (e) {
-        console.error(e)
-        return session?.text('.service_unavailable')
+      if (!options) {
+        return getNews(5)
       }
+      const { category, ragion } = options
+
+      return getNews(5, category, ragion)
     })
 }
--- src/utils.ts
+++ src/utils.ts
@@ -1,33 +1,15 @@
 import { Context } from 'koishi'
-
-export type NewsCategory =
-  | 'topics'
-  | 'notices'
-  | 'maintenance'
-  | 'updates'
-  | 'status'
-  | 'developers'
-export type NewsRagion = 'na' | 'jp' | 'eu' | 'fr' | 'de'
-
-export interface News {
-  id: string
-  title: string
-  url: string
-  time: string
-  image: string
-  description: string
-}
+import get, { Locale, News } from 'lodestone-news'
 
 export const getNews = async (
-  ctx: Context,
   count: number,
-  category: NewsCategory = 'topics',
-  ragion: NewsRagion = 'jp',
-): Promise<News[]> => {
-  const url = `http://${ragion}.lodestonenews.com/news/${category}`
-  const json = (await ctx.http.get(url)) as News[]
-  if (json.length > count) return json.slice(0, count)
-  return json
+  category: string = 'topics',
+  ragion: Locale = 'jp',
+): Promise<string> => {
+  let { news: json } = await get(ragion, category, 1)
+  if (json.length > count) json = json.slice(0, count)
+  return json.map((item) => `${item.title}\n${item.url}`)
+  .join('\n\n')
 }
 
 export const getWebpage = async (
